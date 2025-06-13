ad2e5ec294839ef7ace2d345706e55634609d730
Aly0626 <alyssagracebigar2@gmail.com>
2025-06-13 12:18:10 +0800

    initial commit

---
+++ init.php
@@ -0,0 +1,17 @@
+<?php 
+    define("BASE_URL", "http://localhost/Expense-Manager/");
+    session_start();
+    include_once 'connection.php';
+    include_once 'base.php';
+    include_once 'user.php';
+    include_once 'expense.php';
+    include_once 'budget.php';
+
+    global $pdo;
+
+    $getFromU = new User($pdo);
+    $getFromB = new Budget($pdo);
+    $getFromE = new Expense($pdo);
+?>  
+
+
